cc928dc9c29c298ee1d18f050611fa0d1f5e792f
hassanbelaqdour <hassanbelaqdour@gmail.com>
2025-03-10 13:36:14 +0000

    ajout de la method du register

---
+++ carapi/app/Http/Controllers/AuthController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json(['token' => $token], 201);
+    }
+}
