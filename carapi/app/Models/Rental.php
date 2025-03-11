a8dca28329733b25b2d9f18495010fdaf9f7efc9
hassanbelaqdour <hassanbelaqdour@gmail.com>
2025-03-11 10:13:15 +0000

    ajout du model de rental

--- carapi/app/Models/Rental.php
+++ carapi/app/Models/Rental.php
@@ -3,8 +3,34 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Rental extends Model
 {
-    //
+
+    use HasFactory;
+
+
+    protected $fillable = [
+        'user_id',
+        'car_id',
+        'rental_date',
+        'return_date',
+        'status',
+    ];
+
+
+
+    public function user(){
+        return $this -> belongsTo(user::class);
+    }
+
+    public function car(){
+        return $this ->belongsTo(car::class);
+    }
+
+    public function payments(){
+        return $this -> hasMany(Payment::class);
+    }
+
 }
