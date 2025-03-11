0da77c4b9940ed7da5aaaf363d23e08d823c0480
hassanbelaqdour <hassanbelaqdour@gmail.com>
2025-03-11 10:28:42 +0000

    modification dans routes pour quel soient securises et tester dans postman

--- carapi/routes/api.php
+++ carapi/routes/api.php
@@ -9,7 +9,7 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/register',[AuthController::class, 'register']);
-Route::post('/login',[AuthController::class,'login']);
-Route::post('/logout',[AuthController::class,'logout'])->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 Route::resource('cars', CarController::class);
